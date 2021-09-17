7c5f3bf6a0411b4107c9db7916703493db2f7262
Filipe Silva <filipeqs@outlook.com>
2021-09-17 15:59:55 -0400

    spec to get product by id

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq.Expressions;
 using Core.Entities;
 
 namespace Core.Specifications
@@ -5,6 +7,13 @@ namespace Core.Specifications
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
         public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(q => q.ProductType);
+            AddInclude(q => q.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id) 
+            : base(q => q.ProductTypeId == id)
         {
             AddInclude(q => q.ProductType);
             AddInclude(q => q.ProductBrand);
