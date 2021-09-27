babf35ef3fc4be0808db19221d2dfc5fbbe2c41c
Filipe Silva <filipeqs@outlook.com>
2021-09-27 17:14:43 -0400

    lazi loading

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,21 +6,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    CoreModule,
-    ShopModule,
-    HomeModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        CoreModule,
+        HomeModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -5,11 +5,10 @@ import { SharedModule } from '../shared/shared.module';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from '@angular/router';
+import { ShopRoutingModule } from './shop-routing.module';
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
-  imports: [CommonModule, SharedModule, RouterModule],
-  exports: [ShopComponent],
+    declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
+    imports: [CommonModule, SharedModule, ShopRoutingModule],
 })
 export class ShopModule {}
