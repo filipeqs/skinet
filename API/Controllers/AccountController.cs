b7160d019d6a398e1bc6bd3c5d5eaad4e89e8a8f
Filipe Silva <filipeqs@outlook.com>
2021-10-07 10:53:59 -0400

    update address and addressdto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using API.Dtos;
 using API.Errors;
 using API.Extensions;
+using AutoMapper;
 using Core.Entities.Identity;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -16,14 +17,17 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         public readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
 
         public AccountController(
             UserManager<AppUser> userManager, 
             SignInManager<AppUser> signInManager,
-            ITokenService tokenService)
+            ITokenService tokenService,
+            IMapper mapper)
         {
             _signInManager = signInManager;
             _tokenService = tokenService;
+            _mapper = mapper;
             _userManager = userManager;
         }
 
@@ -49,11 +53,26 @@ namespace API.Controllers
 
         [HttpGet("address")]
         [Authorize]
-        public async Task<ActionResult<Address>> GetUserAddress()
+        public async Task<ActionResult<AddressDto>> GetUserAddress()
         {
             var user = await _userManager.FindUserByEmailClaimsPrincipalWithAddressAsync(User);
 
-            return user.Address; 
+            return _mapper.Map<AddressDto>(user.Address); 
+        }
+
+        [HttpPut("address")]
+        [Authorize]
+        public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto addressDto)
+        {
+            var user = await _userManager.FindUserByEmailClaimsPrincipalWithAddressAsync(User);
+
+            user.Address = _mapper.Map<Address>(addressDto);
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if (result.Succeeded) return Ok(_mapper.Map<AddressDto>(user.Address));
+            
+            return BadRequest("Problem updating the user");
         }
 
         [HttpPost("login")]
