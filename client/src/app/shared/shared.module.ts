e5f90fe03bf87efa5a7d0748428a10dba76d0a96
Filipe Silva <filipeqs@outlook.com>
2021-10-11 20:06:38 -0400

    login user

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -6,16 +6,24 @@ import { PagingHeaderComponent } from './components/paging-header/paging-header.
 import { PagerComponent } from './components/pager/pager.component';
 import { CoreModule } from '../core/core.module';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
-    imports: [CommonModule, CoreModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
+    imports: [
+        CommonModule,
+        CoreModule,
+        PaginationModule.forRoot(),
+        CarouselModule.forRoot(),
+        ReactiveFormsModule,
+    ],
     exports: [
         PaginationModule,
         PagingHeaderComponent,
         PagerComponent,
         CarouselModule,
         OrderTotalsComponent,
+        ReactiveFormsModule,
     ],
 })
 export class SharedModule {}
