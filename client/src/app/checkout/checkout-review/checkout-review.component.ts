b7e997c28dec565f13055246b222d1b2f1fb8ea1
Filipe Silva <filipeqs@outlook.com>
2021-10-26 15:40:18 -0400

    adding paument intent function

--- client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
-  selector: 'app-checkout-review',
-  templateUrl: './checkout-review.component.html',
-  styleUrls: ['./checkout-review.component.scss']
+    selector: 'app-checkout-review',
+    templateUrl: './checkout-review.component.html',
+    styleUrls: ['./checkout-review.component.scss'],
 })
 export class CheckoutReviewComponent implements OnInit {
+    constructor(private basketService: BasketService, private toastr: ToastrService) {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    createPaymentIntent() {
+        return this.basketService.createPaymentIntent().subscribe(
+            (response: any) => {
+                this.toastr.success('Payment intent created');
+            },
+            (error) => {
+                console.log(error);
+                this.toastr.error(error.message);
+            },
+        );
+    }
 }
